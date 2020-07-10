69de8dc56e5fbea0465122f564f96973419b22af
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-10 08:53:13 -0400

    completed creating clusters ips for all services
    completed creating clusters ips for all services

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -24,14 +24,14 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
       content,
       postId: req.params.id,
-      status: 'pending'
-    }
+      status: 'pending',
+    },
   });
 
   res.status(201).send(comments);
@@ -46,19 +46,19 @@ app.post('/events', async (req, res) => {
     const { postId, id, status, content } = data;
     const comments = commentsByPostId[postId];
 
-    const comment = comments.find(comment => {
+    const comment = comments.find((comment) => {
       return comment.id === id;
     });
     comment.status = status;
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentUpdated',
       data: {
         id,
         status,
         postId,
-        content
-      }
+        content,
+      },
     });
   }
 

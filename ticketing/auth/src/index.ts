9aacb5bf5f7fd88a3690e3febc861c4141b703ca
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-15 07:13:10 -0400

    added not found error and implemented it in index.ts
    added not found error and implemented it in index.ts

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -6,12 +6,16 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
 app.use(signupRouter);
+app.all('*', () => {
+  throw new NotFoundError();
+});
 app.use(errorHandler);
 
 app.listen(3000, () => {

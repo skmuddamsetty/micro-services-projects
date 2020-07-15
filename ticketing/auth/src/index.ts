cf184256c337bddf5a098da3eba9b09aa012a278
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-15 07:56:35 -0400

    added mongoose connection in index.ts

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -8,6 +8,7 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(json());
@@ -21,6 +22,20 @@ app.all('*', async (req, res, next) => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Auth Service started and Listening on port 3000!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongo db in auth service!');
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log('Auth Service started and Listening on port 3000!');
+  });
+};
+
+start();

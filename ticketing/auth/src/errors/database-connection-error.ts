a534fd5f7607507d919bc62e63fd8c8e4e129539
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-14 22:48:22 -0400

    moved error transformation logic to error classes
    moved error transformation logic to error classes

--- ticketing/auth/src/errors/database-connection-error.ts
+++ ticketing/auth/src/errors/database-connection-error.ts
@@ -1,8 +1,13 @@
 export class DatabaseConnectionError extends Error {
   reason = 'Error connecting to database!';
+  statusCode = 500;
   constructor() {
     super();
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- ticketing/auth/src/errors/request-validation-error.ts
+++ ticketing/auth/src/errors/request-validation-error.ts
@@ -1,9 +1,17 @@
 import { ValidationError } from 'express-validator';
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
   constructor(public errors: ValidationError[]) {
     super();
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    const formattedErrors = this.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    return formattedErrors;
   }
 }

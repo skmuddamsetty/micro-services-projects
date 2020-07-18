bcff3a17679ae1c132f10227b8a221a09280b968
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-18 09:14:50 -0400

    completed till generation of jwt

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,8 +12,9 @@ import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
-
+app.use(cookieSession({ signed: false, secure: true }));
 app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -4,6 +4,7 @@ import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 router.post(
   '/api/users/signup',
@@ -26,6 +27,9 @@ router.post(
     }
     const user = User.build({ email, password });
     await user.save();
+    // Generate JSONWebToken
+    const userJwt = jwt.sign({ id: user.id, email: user.email }, 'secretkey');
+    req.session = { jwt: userJwt };
     res.status(201).send(user);
     console.log('Creating a user...');
   }

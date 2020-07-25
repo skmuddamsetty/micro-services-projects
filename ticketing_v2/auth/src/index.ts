eb14ddde29cee76ae522b1245002706cdd690123
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-25 07:58:53 -0400

    handling unknown routes

--- ticketing_v2/auth/src/index.ts
+++ ticketing_v2/auth/src/index.ts
@@ -5,6 +5,7 @@ import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
@@ -16,6 +17,11 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+// handling unknown routes
+app.all('*', () => {
+  throw new NotFoundError();
+});
 
 // Error Handler
 app.use(errorHandler);

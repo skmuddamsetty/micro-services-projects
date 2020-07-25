20cdfc8182d1ed4fa01b155f6a456d13d3badfda
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-24 21:53:57 -0400

    added error handler middleware

--- ticketing_v2/auth/src/index.ts
+++ ticketing_v2/auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 
@@ -15,6 +16,9 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+// Error Handler
+app.use(errorHandler);
 
 // server start
 app.listen(3000, () => {
--- ticketing_v2/auth/src/routes/signup.ts
+++ ticketing_v2/auth/src/routes/signup.ts
@@ -13,8 +13,9 @@ router.post(
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error('Invalid email or password');
     }
+    throw new Error('Error connecting to db!');
     const { email, password } = req.body;
     res.send('Hi There!');
   }

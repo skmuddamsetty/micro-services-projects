3537286c38e1c3430c528c2ffc26463bae3d78e6
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-25 09:21:03 -0400

    added logic to add user to mongodb

--- ticketing_v2/auth/src/routes/signup.ts
+++ ticketing_v2/auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
 const router = express.Router();
+
 router.post(
   '/api/users/signup',
   [
@@ -12,14 +13,20 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters!'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    throw new DatabaseConnectionError();
     const { email, password } = req.body;
-    res.send('Hi There!');
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log('Email in use!');
+      return res.send({});
+    }
+    const user = User.build({ email, password });
+    await user.save();
+    res.status(200).send(user);
   }
 );
 

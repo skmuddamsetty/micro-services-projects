01e10d8d747f9a2d1981efd19940639b86bf2209
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-25 09:10:21 -0400

    completed user model

--- ticketing_v2/auth/src/models/user.ts
+++ ticketing_v2/auth/src/models/user.ts
@@ -1,5 +1,27 @@
 import mongoose from 'mongoose';
 
+// an interface that describes the properties that are required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// an interface that describes the properties a User Model has
+// this is required because we have to tell typescript that there is a build function available on this model
+// without this, if you are accessing User.build({}), typescript is going to complain that it cannot find the build method
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(userAttrs: UserAttrs): UserDoc;
+}
+
+// an interface that describes the properties a User Document has
+// this is useful to add more properties to the UserDocument which will have additional properties created by mongoose. for ex: createdAt, updatedAt
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  // createdAt: string;
+  // updatedAt: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,7 +33,13 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// defining static properties on userSchema
+// in other words adding a function to a model
+userSchema.statics.build = (userAttrs: UserAttrs) => {
+  return new User(userAttrs);
+};
+
 // creating a model from schema
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };

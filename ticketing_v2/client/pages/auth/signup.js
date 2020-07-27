8e1e2b3cf8c82de4cb30b2ddb83d0ca3464382f6
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-27 16:02:35 -0400

    added state handling to signup form

--- ticketing_v2/client/pages/auth/signup.js
+++ ticketing_v2/client/pages/auth/signup.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+
 export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className='form-group'>
         <label htmlFor=''>Email Address</label>
-        <input type='text' className='form-control' />
+        <input
+          type='text'
+          className='form-control'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className='form-group'>
         <label htmlFor=''>Password</label>
-        <input type='password' className='form-control' />
+        <input
+          type='password'
+          className='form-control'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <button className='btn btn-primary'>Sign Up</button>
     </form>

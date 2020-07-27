d52acbe1a84f23bd75233c7f76a3c30d124c6fb4
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-26 23:29:04 -0400

    added error handling middleware

--- ticketing_v2/auth/src/routes/signin.ts
+++ ticketing_v2/auth/src/routes/signin.ts
@@ -1,7 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
+
 const router = express.Router();
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi There!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').trim().isEmail().withMessage('Email must be valid.'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('Please provide a password!'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    res.send({});
+  }
+);
 
 export { router as signInRouter };

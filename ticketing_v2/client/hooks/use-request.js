f221d598477c5eaafff183a64e9056f6eeec97c4
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-27 16:34:21 -0400

    created useRequest hook

---
+++ ticketing_v2/client/hooks/use-request.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { useState } from 'react';
+export default ({ url, method, body }) => {
+  const [errors, setErrors] = useState([]);
+  const doRequest = async () => {
+    try {
+      const response = await axios[method](url, body);
+      return response.data;
+    } catch (error) {
+      setErrors(
+        <div className='alert alert-danger'>
+          <ul className='my-0'>
+            {error.response.data.errors.map((err) => {
+              return <li key={err.message}>{err.message}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    }
+  };
+  return { doRequest, errors };
+};
--- ticketing_v2/client/pages/auth/signup.js
+++ ticketing_v2/client/pages/auth/signup.js
@@ -1,21 +1,17 @@
 import { useState } from 'react';
-import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    method: 'post',
+    url: '/api/users/signup',
+    body: { email, password },
+  });
   const onSubmit = async (event) => {
     event.preventDefault();
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      });
-      console.log(response.data);
-    } catch (error) {
-      setErrors(error.response.data.errors);
-    }
+    doRequest();
   };
   return (
     <form onSubmit={onSubmit}>
@@ -38,16 +34,7 @@ export default () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      {errors.length > 0 && (
-        <div className='alert alert-danger'>
-          <ul className='my-0'>
-            {errors.map((err) => {
-              return <li key={err.message}>{err.message}</li>;
-            })}
-          </ul>
-        </div>
-      )}
-
+      {errors}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
   );

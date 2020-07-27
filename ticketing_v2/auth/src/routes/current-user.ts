7b176c7c9cb7b7470bd58e5a15bdf54b1f1e61a3
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-26 23:43:02 -0400

    added current user route

--- ticketing_v2/auth/src/routes/current-user.ts
+++ ticketing_v2/auth/src/routes/current-user.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 const router = express.Router();
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi There!');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };

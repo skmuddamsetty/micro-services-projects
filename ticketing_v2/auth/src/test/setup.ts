43904176089e73399bf5f5f0ad755577cc624748
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-27 14:34:48 -0400

    completed unit tests for current user route

--- ticketing_v2/auth/src/test/setup.ts
+++ ticketing_v2/auth/src/test/setup.ts
@@ -1,7 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'awawd';
@@ -24,3 +32,14 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+};

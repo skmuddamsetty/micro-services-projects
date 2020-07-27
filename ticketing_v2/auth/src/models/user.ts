b235f13420a47dda0106e9cdcb1abd48b04dff23
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-26 23:13:06 -0400

    formatted json properties

--- ticketing_v2/auth/src/models/user.ts
+++ ticketing_v2/auth/src/models/user.ts
@@ -23,16 +23,28 @@ interface UserDoc extends mongoose.Document {
   // updatedAt: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 // defining static properties on userSchema
 // in other words adding a function to a model

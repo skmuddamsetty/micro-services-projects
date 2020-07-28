d2f1d7c9cca53fd591ca95d5ca8590315f31021d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-28 07:25:30 -0400

    refactored code inside getInitialprops to a common file

--- ticketing_v2/client/pages/index.js
+++ ticketing_v2/client/pages/index.js
@@ -1,25 +1,14 @@
 import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Hello From Next!</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on server
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    // we are on the browser
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;

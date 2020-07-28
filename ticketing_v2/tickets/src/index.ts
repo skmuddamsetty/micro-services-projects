0264fad48985b2285f01441de6f03da2f5a62b2e
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-28 15:33:48 -0400

    added MONGO_URI environment variable in depl files for auth and tickets

--- ticketing_v2/tickets/src/index.ts
+++ ticketing_v2/tickets/src/index.ts
@@ -6,19 +6,22 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined!');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined!');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log('Connected to MongoDB inside Auth application!');
+    console.log('Connected to MongoDB inside Tickets application!');
   } catch (error) {
     console.error(error);
   }
   // server start
   app.listen(3000, () => {
-    console.log('Auth Service listening on port 3000!');
+    console.log('Tickets Service listening on port 3000!');
   });
 };
 

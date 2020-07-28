a8904ec9beeca96f7616e728786677a6cb95e3f1
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-28 07:32:46 -0400

    completed signin form

--- ticketing_v2/client/pages/index.js
+++ ticketing_v2/client/pages/index.js
@@ -1,9 +1,11 @@
-import axios from 'axios';
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Hello From Next!</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {

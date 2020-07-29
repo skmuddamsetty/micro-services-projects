45b28ff1a8133d316eda4b251991310f28c5b28d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-28 23:42:08 -0400

    added sequence number to listener

--- ticketing_v2/nats-test/src/listener.ts
+++ ticketing_v2/nats-test/src/listener.ts
@@ -1,11 +1,16 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
 console.clear();
 // create a client
 const stan = nats.connect('ticketing', '123', { url: 'http://localhost:4222' });
 stan.on('connect', () => {
   console.log('Listener connected to nats!');
   const subscription = stan.subscribe('ticket:created');
-  subscription.on('message', (msg) => {
-    console.log('Message Received!');
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+    if (typeof data === 'string') {
+      console.log(
+        `Received Event Number ${msg.getSequence()}, with data ${data}`
+      );
+    }
   });
 });

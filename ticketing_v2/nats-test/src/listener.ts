8aa63d1d47fc018d0d9c78262f1f783286ecc30a
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-29 09:51:33 -0400

    durable subscription, deliverallavailable, queue group

--- ticketing_v2/nats-test/src/listener.ts
+++ ticketing_v2/nats-test/src/listener.ts
@@ -9,15 +9,36 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 });
 stan.on('connect', () => {
   console.log('Listener connected to nats!');
+
+  // close is going to tell nats to not send this listener any more messages
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+  // manualAckMode tells nats to not automaticall aknowledge that the event has been sucessfull
+  // it is up to the user to acknowledge the msg to nats after processing the events successfully
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('orders-service');
   const subscription = stan.subscribe(
     'ticket:created',
     // orders-service-queue-group is a queue group which makes sure that the event is sent to only one listener which are attached to this queue group
-    'orders-service-queue-group'
+    'orders-service-queue-group',
+    options
   );
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
     if (typeof data === 'string') {
       console.log(`Received Event # ${msg.getSequence()}, with data ${data}`);
     }
+    // acknowledging the msg
+    msg.ack();
   });
 });
+
+// intercepting if a listener has been interrupted by using rs or control + c
+// when this happens we are calling the stan.close subscription for a graceful exit
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());

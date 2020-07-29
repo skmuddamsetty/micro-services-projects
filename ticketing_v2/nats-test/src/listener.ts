df232359f83879e5c33d515c86b18e09dfdf6d01
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-28 23:49:02 -0400

    random id for client id in listener

--- ticketing_v2/nats-test/src/listener.ts
+++ ticketing_v2/nats-test/src/listener.ts
@@ -1,7 +1,12 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
 console.clear();
 // create a client
-const stan = nats.connect('ticketing', '123', { url: 'http://localhost:4222' });
+// second argument here is clientid and the name is 123
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
 stan.on('connect', () => {
   console.log('Listener connected to nats!');
   const subscription = stan.subscribe('ticket:created');
--- ticketing_v2/nats-test/src/publisher.ts
+++ ticketing_v2/nats-test/src/publisher.ts
@@ -1,6 +1,7 @@
 import nats from 'node-nats-streaming';
 console.clear();
 // create a client
+// second argument here is clientid and the name is abc
 const stan = nats.connect('ticketing', 'abc', { url: 'http://localhost:4222' });
 
 // below function is executed after the client is successfully connected to the nats streaming server

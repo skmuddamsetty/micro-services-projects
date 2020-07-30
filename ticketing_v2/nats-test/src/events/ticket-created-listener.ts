df958f3eef46c050dfc966dada863b0679483ff7
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-30 08:52:42 -0400

    comments for ticket-created-listener

--- ticketing_v2/nats-test/src/events/ticket-created-listener.ts
+++ ticketing_v2/nats-test/src/events/ticket-created-listener.ts
@@ -3,7 +3,9 @@ import { Message } from 'node-nats-streaming';
 import { TicketCreatedEvent } from './ticket-created-event';
 import { Subjects } from './subjects';
 
+// tight coupling here is done with the help of using the TicketCreatedEvent interface in the generic
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  // here we are first defining the subject to be of type Subjects.TicketCreated and then assigning the value of Subjects.TicketCreated on purpose so that subject is exactly always equal to Subjects.TicketCreated
   readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
   // implementing onMessage

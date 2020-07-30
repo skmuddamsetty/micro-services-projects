c2d959b39dd02d3db3190e9c5263cf3834fd174d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-29 20:43:09 -0400

    refactored the code to seperate classes

---
+++ ticketing_v2/nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,11 @@
+import { Listener } from './base-listener';
+import { Message } from 'node-nats-streaming';
+
+export class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', msg.getSequence(), data);
+    msg.ack();
+  }
+}

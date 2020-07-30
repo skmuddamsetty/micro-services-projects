bcfb12a35a5fc166d4775bb97f2318db5fe637cb
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-30 08:16:23 -0400

    comments for subjects

--- ticketing_v2/nats-test/src/events/subjects.ts
+++ ticketing_v2/nats-test/src/events/subjects.ts
@@ -1,4 +1,5 @@
 // subject is the name of the channel inside world of nats
+// this enum is going to list out all valid subject names we are going to receive
 export enum Subjects {
   TicketCreated = 'ticket:created',
   OrderUpdated = 'order:updated',

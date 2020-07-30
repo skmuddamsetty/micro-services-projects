40a3baf15b50e1994cd717ab11a00277fcd7589d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-30 16:42:00 -0400

    accessing nats client

--- ticketing_v2/tickets/src/routes/new.ts
+++ ticketing_v2/tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@skmtickets/common';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -21,12 +22,12 @@ router.post(
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
     // publishing an event
-    // new TicketCreatedPublisher(client).publish({
-    //   id: ticket.id,
-    //   title: ticket.title,
-    //   price: ticket.price,
-    //   userId: ticket.userId,
-    // });
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     res.status(201).send(ticket);
   }
 );

39c5c5b9416cc748c15887e1ad1b15bd6bd0537c
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-30 00:03:21 -0400

    enforcing data types

--- ticketing_v2/nats-test/src/events/ticket-created-listener.ts
+++ ticketing_v2/nats-test/src/events/ticket-created-listener.ts
@@ -4,9 +4,9 @@ import { TicketCreatedEvent } from './ticket-created-event';
 import { Subjects } from './subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', msg.getSequence(), data);
     msg.ack();
   }

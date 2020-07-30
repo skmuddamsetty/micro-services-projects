fc4bdd31ef3b82f0e7af2cc658d0cef9f4357598
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-30 07:13:07 -0400

    completed till using custom publisher

--- ticketing_v2/nats-test/src/publisher.ts
+++ ticketing_v2/nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 console.clear();
 // create a client
 // second argument here is clientid and the name is abc
@@ -7,12 +8,19 @@ const stan = nats.connect('ticketing', 'abc', { url: 'http://localhost:4222' });
 // below function is executed after the client is successfully connected to the nats streaming server
 stan.on('connect', () => {
   console.log('Publisher connected to nats!');
-  const data = JSON.stringify({
-    id: 'awdawdawd',
+  /********** commenting old logic */
+  // const data = JSON.stringify({
+  //   id: 'awdawdawd',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event Published!');
+  // });
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
+    id: 'jbskjdba123',
     title: 'concert',
     price: 20,
-  });
-  stan.publish('ticket:created', data, () => {
-    console.log('Event Published!');
   });
 });
